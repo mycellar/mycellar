52a8ae6d2ad8053d47141f05af1ec8992b735bfd
speralta <sebastien@peralta.fr>
2010-11-01 12:16:17 +0100

    Domain: add Format.

---
+++ mycellar-java/mycellar-domain/src/main/java/fr/peralta/mycellar/domain/wine/Format.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2011, MyCellar
+ *
+ * This file is part of MyCellar.
+ *
+ * MyCellar is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyCellar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyCellar. If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.peralta.mycellar.domain.wine;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.apache.commons.lang3.ObjectUtils;
+
+import fr.peralta.mycellar.domain.shared.NamedEntity;
+
+/**
+ * @author speralta
+ */
+@Entity
+@Table(name = "FORMAT")
+@AttributeOverride(name = "name", column = @Column(name = "NAME", nullable = false, unique = true))
+@SequenceGenerator(name = "FORMAT_ID_GENERATOR", allocationSize = 1)
+public class Format extends NamedEntity<Format> {
+
+    private static final long serialVersionUID = 201010311742L;
+
+    @Column(name = "CAPACITY")
+    private float capacity;
+
+    @Id
+    @GeneratedValue(generator = "FORMAT_ID_GENERATOR")
+    @Column(name = "ID", nullable = false, unique = true)
+    private Integer id;
+
+    /**
+     * @param name
+     * @param capacity
+     */
+    public Format(String name, float capacity) {
+        super(name);
+        this.capacity = capacity;
+    }
+
+    /**
+     * Needed by hibernate.
+     */
+    Format() {
+    }
+
+    /**
+     * @return the id
+     */
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @return the capacity
+     */
+    public float getCapacity() {
+        return capacity;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected Object[] getHashCodeData() {
+        return new Object[] { getName() };
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected boolean dataEquals(Format other) {
+        return ObjectUtils.equals(getName(), other.getName());
+    }
+
+}
