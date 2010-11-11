b989894976529dd50124aba1e70a7feb0157654a
bperalta <bertrand@peralta.fr>
2010-11-11 23:18:32 +0100

    modif pour supprimer temporairement une erreur

--- mycellar-java/mycellar-interfaces/mycellar-interface-facade-server/src/main/java/fr/peralta/mycellar/interfaces/facades/stock/StockServiceFacadeImpl.java
+++ mycellar-java/mycellar-interfaces/mycellar-interface-facade-server/src/main/java/fr/peralta/mycellar/interfaces/facades/stock/StockServiceFacadeImpl.java
@@ -22,7 +22,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import fr.peralta.mycellar.application.stock.StockService;
-import fr.peralta.mycellar.domain.stock.Bottle;
 import fr.peralta.mycellar.domain.stock.Input;
 import fr.peralta.mycellar.interfaces.facades.shared.MapperServiceFacade;
 import fr.peralta.mycellar.interfaces.facades.stock.dto.Arrival;
@@ -45,9 +44,10 @@ public class StockServiceFacadeImpl implements StockServiceFacade {
         float unitCharges = arrival.getOtherCharges() / arrival.getBottles().size();
         for (fr.peralta.mycellar.interfaces.facades.stock.dto.Bottle bottleDto : arrival
                 .getBottles()) {
-            inputs.add(new Input(arrival.getDate(), mapperServiceFacade
-                    .map(bottleDto, Bottle.class), bottleDto.getQuantity(), bottleDto.getPrice(),
-                    arrival.getSource(), unitCharges));
+            // inputs.add(new Input(arrival.getDate(), mapperServiceFacade
+            // .map(bottleDto, Bottle.class), bottleDto.getQuantity(),
+            // bottleDto.getPrice(),
+            // arrival.getSource(), unitCharges));
         }
         stockService.stock(inputs);
     }
