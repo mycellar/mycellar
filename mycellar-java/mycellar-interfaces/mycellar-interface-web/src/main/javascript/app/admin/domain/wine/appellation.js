61074200731f64eb99918f1d5246ee37fecececd
speralta <sebastien@peralta.fr>
2013-11-06 19:40:07 +0100

    Fix appelation admin page.

--- mycellar-java/mycellar-interfaces/mycellar-interface-web/src/main/javascript/app/admin/domain/wine/appellation.js
+++ mycellar-java/mycellar-interfaces/mycellar-interface-web/src/main/javascript/app/admin/domain/wine/appellation.js
@@ -1,13 +1,13 @@
 angular.module('mycellar.controllers.admin.domain.wine.appellation', [
   'mycellar.resources.wine.appellations',
-  'mycellar.resources.wine.countries',
+  'mycellar.resources.wine.regions',
   'mycellar.services.admin-domain',
   'mycellar.directives.admin-domain-nav'
 ]);
 
 angular.module('mycellar.controllers.admin.domain.wine.appellation').controller('AdminDomainAppellationController', [
-  '$scope', 'appellation', 'adminDomainService', 'Countries',
-  function ($scope, appellation, adminDomainService, Countries) {
+  '$scope', 'appellation', 'adminDomainService', 'Regions',
+  function ($scope, appellation, adminDomainService, Regions) {
     $scope.appellation = appellation;
     angular.extend($scope, adminDomainService.editMethods('wine', 'Appellation', appellation, 'form'));
     $scope.regions = Regions.nameLike;
