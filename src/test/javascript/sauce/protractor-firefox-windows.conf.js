80b9249ab6458e2771cd3632e89c25acb44d6035
speralta <sebastien@peralta.fr>
2014-01-03 01:07:22 +0100

    fix: concurrent task configuration

--- src/test/javascript/sauce/protractor-firefox-windows.conf.js
+++ src/test/javascript/sauce/protractor-firefox-windows.conf.js
@@ -1,7 +1,30 @@
-var shared = require('../protractor.conf');
-shared.capabilities.browserName = ['firefox'];
-shared.capabilities.platform = ['Windows 8.1'];
+exports.config = {
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
 
-module.exports = function(config) {
-  config.set(shared);
+  specs: [
+    './e2e/**/*.js'
+  ],
+
+  capabilities: {
+    'browserName': 'firefox',
+    'platform': 'Windows 8.1',
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'Travis #' + process.env.TRAVIS_BUILD_NUMBER,
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+  },
+
+  baseUrl: 'http://localhost:8080',
+  jasmineNodeOpts: {
+    // onComplete will be called just before the driver quits.
+    onComplete: null,
+    // If true, display spec names.
+    isVerbose: false,
+    // If true, print colors to the terminal.
+    showColors: false,
+    // If true, include stack traces in failures.
+    includeStackTrace: true,
+    // Default time to wait in ms before a test fails.
+    defaultTimeoutInterval: 60000
+  }
 };
