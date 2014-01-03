10db9c36f1c00254e14ed624c455653cdce80253
speralta <sebastien@peralta.fr>
2014-01-03 16:46:43 +0100

    chore: remove old unused services.

--- src/main/javascript/shared/services/bootstrap/bootstrap.js
+++
@@ -1,5 +0,0 @@
-angular.module('mycellar.services.bootstrap', [
-  'mycellar.services.bootstrap.transition',
-  'mycellar.services.bootstrap.typeahead',
-  'mycellar.services.bootstrap.position'
-]);
