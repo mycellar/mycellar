9b4f8cd43edecec7af9cd28fbcf87f45e856c1b2
speralta <sebastien@peralta.fr>
2014-02-16 17:46:56 +0100

    update dependencies

--- src/main/java/fr/mycellar/MyCellarApplication.java
+++ src/main/java/fr/mycellar/MyCellarApplication.java
@@ -22,7 +22,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.SpringBootServletInitializer;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
