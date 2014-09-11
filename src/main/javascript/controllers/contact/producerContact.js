e66aef0741d63a0bc55b6c6318edea9079374e44
speralta <sebastien@peralta.fr>
2014-09-11 14:25:31 +0200

    feat: contact WIP

--- src/main/javascript/controllers/contact/producerContact.js
+++ src/main/javascript/controllers/contact/producerContact.js
@@ -6,20 +6,30 @@ angular.module('mycellar.controllers.contact.producer', [
 ], [
   '$routeProvider',
   function($routeProvider) {
-    $routeProvider.when('/contact/producer/:producerId', {
+    $routeProvider.when('/contact/producer/:producerId?', {
       templateUrl: 'partials/views/contact/producerContact.tpl.html',
       controller: 'ProducerContactController',
       resolve: {
         producer: ['$route', 'AdminProducers', function ($route, Producers) {
-          return Producers.get({id: $route.current.params.producerId}).$promise;
+          var producerId = $route.current.params.producerId;
+          if (producerId) {
+            return Producers.get({id: producerId}).$promise;
+          } else {
+            return new Producers();
+          }
         }],
         contacts: ['$route', 'AdminContacts', function ($route, Contacts) {
-          return Contacts.get({
-            first: 0,
-            count: 10,
-            sort: ['current,desc'],
-            filters: ['producer.id,' + $route.current.params.producerId]
-          }).$promise;
+          var producerId = $route.current.params.producerId;
+          if (producerId) {
+            return Contacts.get({
+              first: 0,
+              count: 5,
+              sort: ['current,desc'],
+              filters: ['producer.id,' + $route.current.params.producerId]
+            }).$promise;
+          } else {
+            return {list: [], count: 0};
+          }
         }]
       }
     });
@@ -32,7 +42,7 @@ angular.module('mycellar.controllers.contact.producer').controller('ProducerCont
     $scope.more = function() {
       var parameters = {
           first: $scope.contacts.length,
-          count: 10,
+          count: 5,
           sort: ['current,desc'],
           filters: ['producer.id,' + $scope.producer.id]
         };
@@ -45,7 +55,14 @@ angular.module('mycellar.controllers.contact.producer').controller('ProducerCont
     $scope.producer = producer;
     $scope.contacts = contacts.list;
     $scope.size = contacts.count;
+    $scope.edit = producer.id == null;
 
+    $scope.modifyProducer = function() {
+      $scope.edit = true;
+      $scope.backup = {};
+      angular.copy($scope.producer, $scope.backup);
+    };
+    
     $scope.saveProducer = function() {
       $scope.errors = [];
       $scope.backup = {};
@@ -58,11 +75,18 @@ angular.module('mycellar.controllers.contact.producer').controller('ProducerCont
             }
           });
           $scope.errors.push(value);
+          angular.copy($scope.backup, $scope.producer);
         } else {
           $scope.backup = undefined;
+          $scope.edit = false;
         }
       });
     };
+
+    $scope.cancelProducer = function() {
+      angular.copy($scope.backup, $scope.producer);
+      $scope.edit = false;
+    }
 
     $scope.deleteContact = function(contact) {
       Contacts.delete(contact);
