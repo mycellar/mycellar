977cb686262692f1b22891e66cf3bf3651c6c354
speralta <sebastien@peralta.fr>
2014-10-01 11:36:20 +0200

    feat: vinopedia

--- src/main/javascript/controllers/vinopedia/pedia.js
+++ src/main/javascript/controllers/vinopedia/pedia.js
@@ -8,16 +8,16 @@ angular.module('mycellar.controllers.vinopedia.pedia', [
       templateUrl: 'partials/views/vinopedia/pedia.tpl.html',
       controller: 'VinopediaController',
       resolve: {
-        tableContext: ['tableService', 'Wines', function(tableService, Wines) {
-          var tableContext = tableService.createTableContext(Wines.get, [
-            'appellation.region.country.name',
-            'appellation.region.name',
-            'appellation.name',
-            'producer.name',
-            'name', 
-            'vintage'
-          ]);
-          return tableContext.setPage(1).promise;
+        wines: ['Wines', function(Wines) {
+          return Wines.get({
+            first: 0,
+            count: 50,
+            sort: [
+              'producer.name,asc',
+              'name,asc',
+              'vintage,asc'
+            ]
+          }).$promise;
         }]
       }
     });
@@ -25,8 +25,63 @@ angular.module('mycellar.controllers.vinopedia.pedia', [
 ]);
 
 angular.module('mycellar.controllers.vinopedia.pedia').controller('VinopediaController', [
-  '$scope', 'tableContext',
-  function ($scope, tableContext) {
-    $scope.tableContext = tableContext;
+  '$scope', 'Wines', 'wines', 'search',
+  function ($scope, Wines, wines, search) {
+    var getWines = function(parameters) {
+      if (parameters.input === $scope.search) {
+        var params = {
+          first: parameters.first,
+          count: $scope.pageCount,
+          sort: [
+            'producer.name,asc',
+            'name,asc',
+            'vintage,asc'
+          ]
+        };
+        if ($scope.search != null && $scope.search != '') {
+          params['like'] = $scope.search;
+        }
+        return Wines.get(params, parameters.callback);
+      }
+    };
+
+    $scope.pageCount = 50;
+    $scope.search = '';
+    if (wines != null) {
+      $scope.wines = wines.list;
+      $scope.size = wines.count;
+    }
+
+    $scope.more = function() {
+      getWines({
+        input: $scope.search,
+        cellar: $scope.cellar,
+        first: $scope.wines.length,
+        callback: function(value) {
+          $scope.wines = $scope.wines.concat(value.list);
+        }
+      });
+    };
+
+    $scope.toggleHidden = function() {
+      search.toggleHidden();
+      $scope.search = '';
+    };
+    $scope.clearSearch = function() {
+      search.clearSearch();
+      $scope.search = '';
+    };
+    $scope.$watch('search', function(newValue, oldValue) {
+      if (newValue !== oldValue) {
+        return search.scheduleSearch(getWines, {
+          input: newValue,
+          first: 0,
+          callback: function(value) {
+            $scope.wines = value.list;
+            $scope.size = value.count;
+          }
+        });
+      }
+    });
   }
 ]);
